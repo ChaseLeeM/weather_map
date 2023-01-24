0989e764b14525cb57842a706b8e83ed64e1303e
Chase Medford <chasemedford1@gmail.com>
2023-01-24 12:15:04 -0600

    Map Works. Weather shows up on map. Cards are kind created but not taking in any data.

--- js/mapbox-geocoder-utils_two.js
+++ js/mapbox-geocoder-utils_two.js
@@ -15,7 +15,7 @@
  */
 function geocode(search, token) {
     var baseUrl = 'https://api.mapbox.com';
-    var endPoint = '/geocoding/v5/mapbox.places/';
+    var endPoint = '/geocoding/v12/mapbox.places/';
     return fetch(baseUrl + endPoint + encodeURIComponent(search) + '.json' + "?" + 'access_token=' + token)
         .then(function(res) {
             return res.json();
@@ -24,6 +24,7 @@ function geocode(search, token) {
             return data.features[0].center;
         });
 }
+
 
 
 /***
