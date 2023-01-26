3b64b9315367805a2463339e6c8b2c6b3f2ab732
Chase Medford <chasemedford1@gmail.com>
2023-01-26 16:05:32 -0600

    Map works, but the cards only populate on a map click. Search works, but it doesn't fill weather cards automatically.

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,33 +1,34 @@
-$(document).ready(function() {
 
 
 mapboxgl.accessToken = keys.mapbox;
-let map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v12',
-    zoom: 10,
-    center: [-98.4916, 29.4252]
+const map = new mapboxgl.Map({
+    container: "map", // container ID
+    style: "mapbox://styles/mapbox/streets-v12", // style URL
+    center: [-98.490415, 29.4267], // starting position [lng, lat]
+    zoom: 9, // starting zoom
 });
 
-
-
-$.get("http://api.openweathermap.org/data/2.5/forecast", {
-    APPID: keys.weatherKey,
-    q:     "San Antonio, US",
-    units: "imperial"
-}).done(function(data) {
-    for (let i = 0; i < data.list.length; i+=8) {
-        let weather = data.list[i];
-        let date = new Date(weather.dt * 1000);
-        let day = date.getDay();
-        let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-        let dayName = days[day];
-        let temp = weather.main.temp;
-        let highLow = weather.main.temp_max + "/" + weather.main.temp_min;
-        let icon = weather.weather[0].icon;
-        let humidity = weather.main.humidity;
-        let description = weather.weather[0].description;
-        let html = `<div class="card col-2">
+function weatherCall(location){
+    // location = $("#searchBar").value();
+    // let token = keys.mapbox;
+    let html = ""
+    // $.get("http://api.openweathermap.org/data/2.5/forecast", {
+    //     APPID: keys.weatherKey,
+    //     q:     location,
+    //     units: "imperial"
+    // }).done(function(data) {
+        for (let i = 0; i < location.list.length; i+=8) {
+            let weather = location.list[i];
+            let date = new Date(weather.dt * 1000);
+            let day = date.getDay();
+            let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+            let dayName = days[day];
+            let temp = weather.main.temp;
+            let highLow = weather.main.temp_max + "/" + weather.main.temp_min;
+            let icon = weather.weather[0].icon;
+            let humidity = weather.main.humidity;
+            let description = weather.weather[0].description;
+            html += `<div class="card col-2">
                       <div class="card-body">
                         <h5 class="card-title">${dayName}</h5>
                             <p class="card-text">${temp}°F</p>
@@ -36,58 +37,48 @@ $.get("http://api.openweathermap.org/data/2.5/forecast", {
                             <p class="card-text">${description}</p>
                             <img src="http://openweathermap.org/img/w/${icon}.png" alt="${description}">
                         </div>
-                        </div>`;
-        $(".weather").append(html);
-    }
-});
-
-function geocode(search, token) {
-    var baseUrl = 'https://api.mapbox.com';
-    var endPoint = '/geocoding/v12/mapbox.places/';
-    return fetch(baseUrl + endPoint + encodeURIComponent(search) + '.json' + "?" + 'access_token=' + token)
-        .then(function(res) {
-            return res.json();
-            // to get all the data from the request, comment out the following three lines...
-        }).then(function(data) {
-            return data.features[0].center;
-        });
-}
-    function reverseGeocode(coordinates, token) {
-        var baseUrl = 'https://api.mapbox.com';
-        var endPoint = '/geocoding/v5/mapbox.places/';
-        return fetch(baseUrl + endPoint + coordinates.lng + "," + coordinates.lat + '.json' + "?" + 'access_token=' + token)
-            .then(function(res) {
-                return res.json();
-            })
-            // to get all the data from the request, comment out the following three lines...
-            .then(function(data) {
-                return data.features[0].place_name;
-            });
-function setMarker(lng, lat) {
-    var marker = new mapboxgl.Marker()
-        .setLngLat([lng, lat])
-        .addTo(map);
-}
+                    </div>`;
+        }
+        $("#weather").html(html);
+    // });
+};
 
 
-$("#searchBtn").click(function(e) {
+document.getElementById("searchBtn").addEventListener('click', function (e) {
     e.preventDefault();
-    let search = $("#searchBar").val();
-    geocode(search, keys.mapbox).then(function(result) {
-        setMarker(result[0], result[1]);
-        map.flyTo({
-            center: result,
-            essential: true
+    let location = $("#searchBar").val();
+    let coordinates = e.lngLat;
+    geocode(location, keys.mapbox).then(function (loc) {
+        $(".mapboxgl-marker").remove();
+        let newMarker = new mapboxgl.Marker({draggable: true}).setLngLat(loc).addTo(map);
+        map.panTo(loc)
+        $.get("http://api.openweathermap.org/data/2.5/forecast", {
+            APPID: keys.weatherKey,
+            lat: coordinates.lat,
+            lon: coordinates.lng,
+            units: "imperial"
+        }).done(function (location) {
+            weatherCall(location);
         });
     });
 });
 
 
-map.on('click', function(e) {
-    reverseGeocode(e.lngLat, keys.mapbox).then(function(result) {
-        alert(result);
+// sets new location by map click
+    map.on('click', (e) => {
+        $(".mapboxgl-marker").remove();
+        let coordinates = e.lngLat;
+        let newMarker = new mapboxgl.Marker().setLngLat(coordinates).addTo(map)
+        console.log(coordinates.lng, coordinates.lat);
+        map.setCenter(coordinates)
+        map.setZoom(10)
+        $.get("http://api.openweathermap.org/data/2.5/forecast", {
+            appid: keys.weatherKey,
+            units: "imperial",
+            lon: coordinates.lng,
+            lat: coordinates.lat,
+        }).done(function (location ) {
+            weatherCall(location);
+        });
     });
 
-
-
-});
