b5558cc129770eb7cdd9a5829fe6c5fd008070f1
Chase Medford <chasemedford1@gmail.com>
2023-01-26 17:19:12 -0600

    No design to speak of, but it works.

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,22 +1,15 @@
-
-
+//Mapbox Call
 mapboxgl.accessToken = keys.mapbox;
 const map = new mapboxgl.Map({
     container: "map", // container ID
     style: "mapbox://styles/mapbox/streets-v12", // style URL
     center: [-98.490415, 29.4267], // starting position [lng, lat]
-    zoom: 9, // starting zoom
+    zoom: 2, // starting zoom
 });
 
+//Function for adding Weather to Cards
 function weatherCall(location){
-    // location = $("#searchBar").value();
-    // let token = keys.mapbox;
     let html = ""
-    // $.get("http://api.openweathermap.org/data/2.5/forecast", {
-    //     APPID: keys.weatherKey,
-    //     q:     location,
-    //     units: "imperial"
-    // }).done(function(data) {
         for (let i = 0; i < location.list.length; i+=8) {
             let weather = location.list[i];
             let date = new Date(weather.dt * 1000);
@@ -40,31 +33,29 @@ function weatherCall(location){
                     </div>`;
         }
         $("#weather").html(html);
-    // });
 };
 
-
+//Sets location by Search bar submit
 document.getElementById("searchBtn").addEventListener('click', function (e) {
     e.preventDefault();
     let location = $("#searchBar").val();
-    let coordinates = e.lngLat;
     geocode(location, keys.mapbox).then(function (loc) {
         $(".mapboxgl-marker").remove();
         let newMarker = new mapboxgl.Marker({draggable: true}).setLngLat(loc).addTo(map);
         map.panTo(loc)
         $.get("http://api.openweathermap.org/data/2.5/forecast", {
             APPID: keys.weatherKey,
-            lat: coordinates.lat,
-            lon: coordinates.lng,
+            q:     location,
             units: "imperial"
         }).done(function (location) {
             weatherCall(location);
+            $('#weather').removeClass("hide");
         });
     });
 });
 
 
-// sets new location by map click
+// Sets New Location By Map Click
     map.on('click', (e) => {
         $(".mapboxgl-marker").remove();
         let coordinates = e.lngLat;
@@ -79,6 +70,7 @@ document.getElementById("searchBtn").addEventListener('click', function (e) {
             lat: coordinates.lat,
         }).done(function (location ) {
             weatherCall(location);
+            $('#weather').removeClass("hide");
         });
     });
 
